4fc5c1843a9a52b5377a0b1917b3f447d3c21f0e
Deusdedit <dbyabatov@gmail.com>
2021-03-31 09:29:46 +0300

    unique receipt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,3 +7,36 @@ composer require barryvdh/laravel-dompdf
 composer require doctrine/dbal
 
 @stop
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UniqueReceiptVoucherToReceivings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('receivings', function (Blueprint $table) {
+            $table->string('receipt_vocher')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('receivings', function (Blueprint $table) {
+            $table->string('receipt_vocher')->nullable(false)->change();
+        });
+    }
+}
